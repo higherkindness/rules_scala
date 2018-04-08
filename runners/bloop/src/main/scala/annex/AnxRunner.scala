a7ae60e351381d0c0a0660b8fe3232e8792e869d
Andy Scott <andy.g.scott@gmail.com>
2018-04-08 14:24:42 -0700

    Dedupe state monad implementation

--- runners/bloop/src/main/scala/annex/AnxRunner.scala
+++ runners/bloop/src/main/scala/annex/AnxRunner.scala
@@ -325,25 +325,3 @@ final class AnxLogger extends Logger {
     println(s"[trace]: ${err.get.getMessage}")
 
 }
-
-// tiny state monad
-
-final case class State[S, A](run: S => (S, A)) {
-
-  def map[B](f: A => B): State[S, B] =
-    new State(s => {
-      val res = run(s)
-      (res._1, f(res._2))
-    })
-
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    new State(s => {
-      val res = run(s)
-      f(res._2).run(res._1)
-    })
-
-}
-
-object State {
-  def pure[S, A](a: A): State[S, A] = State(s => (s, a))
-}
--- runners/common/src/main/scala/annex/Options.scala
+++ runners/common/src/main/scala/annex/Options.scala
@@ -109,3 +109,25 @@ object Options {
     options
   }
 }
+
+// tiny state monad
+
+final case class State[S, A](run: S => (S, A)) {
+
+  def map[B](f: A => B): State[S, B] =
+    new State(s => {
+      val res = run(s)
+      (res._1, f(res._2))
+    })
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    new State(s => {
+      val res = run(s)
+      f(res._2).run(res._1)
+    })
+
+}
+
+object State {
+  def pure[S, A](a: A): State[S, A] = State(s => (s, a))
+}
