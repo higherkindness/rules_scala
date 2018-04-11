af9066d07d95c689ff6b42e1503e156779abda73
Andy Scott <andy.g.scott@gmail.com>
2018-04-11 13:58:05 -0700

    Cleanup misc files

--- gen-deps.sh
+++ gen-deps.sh
@@ -3,6 +3,9 @@ rm -fr 3rdparty
 
 cd "$(dirname "$0")"
 
+echo generating dependencies for main workspace
 external-tools/bazel-deps.sh generate -r "$(pwd)" -s 3rdparty/workspace.bzl -d dependencies.yaml
+echo generating dependencies for tests workspace
+external-tools/bazel-deps.sh generate -r "$(pwd)/tests" -s 3rdparty/workspace.bzl -d dependencies.yaml
 
 ./format.sh
