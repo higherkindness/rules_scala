39318e64e0709b30ea59e1dd499f087c4e27d1e3
Paul Draper <paul@lucidchart.com>
2018-04-26 04:01:42 -0600

    Fix local disk cache for external-tools

--- setup-tools.sh
+++ setup-tools.sh
@@ -1,7 +1,12 @@
-#!/bin/sh -e
+#!/bin/bash -e
 cd "$(dirname "$0")"
 
-BAZEL_OPTS='--experimental_local_disk_cache_path=../.bazel_cache --experimental_strict_action_env'
+BAZEL_OPTS=(
+    --experimental_local_disk_cache
+    --experimental_local_disk_cache_path=../.bazel_cache
+    --experimental_strict_action_env
+)
+mkdir -p external-tools/.bazel_cache
 
 rm -fr external-tools/buildtools
 
@@ -9,7 +14,7 @@ mkdir -p external-tools/buildtools
 echo Downloading buildtools
 curl -L -sS https://github.com/bazelbuild/buildtools/archive/a8cd34f034f2ae1e206eec896cf12d38a0cb26fb.tar.gz | tar zxf - --strip 1 -C external-tools/buildtools
 echo Building buildifier
-(cd external-tools/buildtools; bazel run $BAZEL_OPTS --script_path=../buildifier.sh buildifier)
+(cd external-tools/buildtools; bazel run "${BAZEL_OPTS[@]}" --script_path=../buildifier.sh buildifier)
 
 rm -fr external-tools/bazel-deps
 
@@ -19,4 +24,4 @@ echo Downloading bazel-deps
 curl -L -sS https://github.com/lucidsoftware/bazel-deps/archive/b95e44421a6f1f9ade584154b00a91bf9d53dde9.tar.gz | tar zxf - --strip 1 -C external-tools/bazel-deps
 
 echo Building bazel-deps
-(cd external-tools/bazel-deps; bazel run $BAZEL_OPTS --script_path=../bazel-deps.sh parse)
+(cd external-tools/bazel-deps; bazel run "${BAZEL_OPTS[@]}" --script_path=../bazel-deps.sh parse)
