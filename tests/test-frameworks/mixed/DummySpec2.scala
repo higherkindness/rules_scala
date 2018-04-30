7426d469cc0de36402f54816d2ac8fb77916f33a
Paul Draper <paulddraper@gmail.com>
2018-04-30 14:35:30 -0600

    Test specs2 (#74)

---
+++ tests/test-frameworks/mixed/DummySpec2.scala
@@ -0,0 +1,13 @@
+package annex.specs2
+
+import org.specs2.mutable.Specification
+
+object DummySpec2 extends Specification {
+
+  "Example" should {
+    "work" in {
+      1 must_== 1
+    }
+  }
+
+}
