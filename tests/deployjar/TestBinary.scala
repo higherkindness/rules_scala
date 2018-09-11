6b28e87cdbe0a20184ce24d1db39cc93452069b7
Bor Kae Hwang <borkaehw@lucidchart.com>
2018-09-11 16:22:40 -0600

    Add deploy jar test

---
+++ tests/deployjar/TestBinary.scala
@@ -0,0 +1,12 @@
+package foo
+
+object TestBinary {
+  def main(args: Array[String]) {
+    println("Hello, world1")
+    println("Hello, world2")
+    println("Hello, world3")
+
+    val test = new TestClass
+    test.print
+  }
+}
