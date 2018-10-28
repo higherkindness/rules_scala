3ebc218b485bcd40948010c41adb97a29212eca2
Paul Draper <paulddraper@gmail.com>
2018-10-28 10:06:43 -0600

    Fix regression in direct running of format

--- rules/scalafmt/private/format.template.sh
+++ rules/scalafmt/private/format.template.sh
@@ -1,14 +1,15 @@
 #!/bin/bash -e
 RUNPATH="${TEST_SRCDIR-$0.runfiles}"/%workspace%
+WORKSPACE_ROOT="${1:-$BUILD_WORKSPACE_DIRECTORY}"
 
 while read original formatted; do
     if ! cmp -s "$RUNPATH/$original" "$RUNPATH/$formatted"; then
         echo $original
-        if [ -z "$BUILD_WORKSPACE_DIRECTORY" ]; then
+        if [ -z "$WORKSPACE_ROOT" ]; then
             diff "$RUNPATH/$original" "$RUNPATH/$formatted" || true
             EXIT=1
         else
-            cp "$RUNPATH/$formatted" "$1/$original"
+            cp "$RUNPATH/$formatted" "$WORKSPACE_ROOT/$original"
         fi
     fi
 done < "$RUNPATH"/%manifest%
