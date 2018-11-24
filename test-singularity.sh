30af99d3ea257d184553393b4ff1e0b9b87f193a
Andy Scott <andy.g.scott@gmail.com>
2018-11-24 11:30:24 -0800

    Test against a source build of the scala ecosystem

---
+++ test-singularity.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -e
+
+sha=0ddc53be490bfd068162bcf29a15d5c5183af247
+url="https://github.com/andyscott/singularity/archive/$sha.tar.gz"
+location="singularity"
+if [ ! -d "$location" ]; then
+  mkdir -p "$location"
+  curl -L "$url" | tar zxf - --strip-components=1 -C "$location"
+fi
+
+rules_scala_annex=$(pwd)
+(
+    cd "$location" && \
+    bazel test \
+      --override_repository=rules_scala_annex="$rules_scala_annex" \
+      --config=quick ...
+)
--- tools/gen-ignores.sh
+++ tools/gen-ignores.sh
@@ -3,10 +3,11 @@ cd "$(dirname "$0")"/..
 
 PACKAGES0=$(find -L external-tools -iname BUILD -o -iname BUILD.bazel | xargs -n1 dirname | tr '\n' ,)
 PACKAGES1=$(find -L tests -iname BUILD -o -iname BUILD.bazel | xargs -n1 dirname | tr '\n' ,)
+PACKAGES2=$(find -L singularity -iname BUILD -o -iname BUILD.bazel | xargs -n1 dirname | tr '\n' ,)
 
 echo Updating tools/deleted_packages.rc
 echo '# generated' > tools/deleted_packages.rc
-packages=$PACKAGES0,$PACKAGES1
+packages=$PACKAGES0,$PACKAGES1,$PACKAGES2
 (
     echo "build --deleted_packages=$packages"
     echo "query --deleted_packages=$packages"
