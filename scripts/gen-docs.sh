123d62ae912dc81713725ccf2795e1e881684d13
Andy Scott <andy.g.scott@gmail.com>
2019-01-14 01:38:20 -0800

    :shrug: again

--- scripts/gen-docs.sh
+++ scripts/gen-docs.sh
@@ -12,4 +12,6 @@ mkdir -p docs/stardoc
 
 bazel build rules:docs
 tar xf "$(bazel info bazel-bin)/rules/docs.tar" -C docs/stardoc
-find docs/stardoc -size 0 -print0 | xargs -0 rm --
+find docs/stardoc -size 0 -print0 | while read path; do
+    rm $path
+done
