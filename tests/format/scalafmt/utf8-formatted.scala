c30e4cabfe8eefbef423954ebc99b0bc41aa6564
Bor Kae Hwang <borkaehw@lucidchart.com>
2019-03-13 16:30:43 -0600

    Support utf-8 formatting

---
+++ tests/format/scalafmt/utf8-formatted.scala
@@ -0,0 +1,12 @@
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Be careful with this test")
+    println("小心這個測試")
+    println("このテストに注意してください")
+    println("이 시험에 조심하십시오")
+    println("كن حذرا مع هذا الاختبار")
+    println("Hãy cẩn thận với bài kiểm tra này")
+    println("Будьте осторожны с этим тестом")
+    println("😁✊🚀🍟💯")
+  }
+}
