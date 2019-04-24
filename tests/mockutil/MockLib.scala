4deeaa74ad0c96b48e4c9d5f41f5b9dd7079cc2e
Sergio Rodriguez Orellana <sergio@lucidchart.com>
2019-04-23 18:24:26 -0600

    Fix for issue 178

---
+++ tests/mockutil/MockLib.scala
@@ -0,0 +1,11 @@
+package mockutil
+
+/** Mocklib
+ *
+ * Utility functions
+ */
+object MockLib {
+  def add(i1: Int, i2: Int): Int = {
+    i1 + i2
+  }
+}
--- tests/scaladoc/A.scala
+++ tests/scaladoc/A.scala
@@ -1,6 +1,9 @@
+import mockutil.MockLib
+
 /**
  * A Scaladoc
  */
 object A {
     def a = B
+    def addOne(i: Int) = MockLib.add(i, 1)
 }
