434a61b6be7d2cc7142993cf6b77f77a5e2bea1f
Corey Woodfield <cwoodfield@lucidchart.com>
2022-06-01 17:59:03 -0600

    Handle absence of `::` or multiple instances of `::` in specs2 test_filters

--- tests/test-frameworks/specs2/Specs2TestFilter.scala
+++ tests/test-frameworks/specs2/Specs2TestFilter.scala
@@ -5,7 +5,19 @@ import org.specs2.mutable.Specification
 object Specs2TestFilter extends Specification {
   "scala_test" should {
     "properly (escape) [special] characters in the test filter" in {
-      1 must_== 1
+      true must beTrue
+    }
+
+    "handle tests without scopes" in {
+      true must beTrue
+    }
+
+    "test with :: in name" in {
+      true must beTrue
+    }
+
+    "other test with :: in name" in {
+      true must beTrue
     }
   }
 }
